4a0974bf21081324892490ce8a58f15c506d81ee
杨培文 <915505626@qq.com>
2016-05-28 00:10:44 +0800

    Initial commit

---
+++ app/src/main/java/com/yangpeiwen/myappportfolio/MainA.java
@@ -0,0 +1,13 @@
+package com.yangpeiwen.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
