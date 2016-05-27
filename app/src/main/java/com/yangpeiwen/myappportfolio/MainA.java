2eb9c0f82f9a465f538f16e428e2e360cce541d1
杨培文 <915505626@qq.com>
2016-05-28 00:46:00 +0800

    1

--- app/src/main/java/com/yangpeiwen/myappportfolio/MainA.java
+++ app/src/main/java/com/yangpeiwen/myappportfolio/MainA.java
@@ -2,6 +2,9 @@ package com.yangpeiwen.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainA extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainA extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(View v){
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "This button will launch my " + ((Button)v).getText().toString() + " app.", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
